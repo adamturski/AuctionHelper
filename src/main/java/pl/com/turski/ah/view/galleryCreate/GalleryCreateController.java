9417122eea45a1328ea35959bd7bdde8d969a36d
Adam <adam.turski1@gmail.com>
2014-06-12 19:13:55 +0200

    Refactoring GalleryCreateController

--- src/main/java/pl/com/turski/ah/view/galleryCreate/GalleryCreateController.java
+++ src/main/java/pl/com/turski/ah/view/galleryCreate/GalleryCreateController.java
@@ -7,8 +7,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pl.com.turski.ah.service.ImageService;
 import pl.com.turski.ah.view.ViewController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * User: Adam
@@ -27,11 +33,28 @@ public class GalleryCreateController implements ViewController {
     @FXML
     Node view;
 
+    @Autowired
+    private ImageService imageService;
+
+    private File rootFolder;
+    private List<File> images;
     private boolean galleryCreated;
 
-    public void galleryCreateButtonAction(ActionEvent event) {
+    public void initController(File rootFolder, List<File> images) {
+        this.rootFolder = rootFolder;
+        this.images = images;
+    }
 
-        galleryCreated = true;
+    public void galleryCreateButtonAction(ActionEvent event) {
+        try {
+            progressPane.setVisible(true);
+            File galleryDirectory = new File(rootFolder, "upload");
+            galleryDirectory.mkdir();
+            imageService.generateGallery(galleryDirectory, images);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
     }
 
     public Node getView() {
